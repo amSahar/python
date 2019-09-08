bd90674e6f999f0a7d5736a21d03c58610ee42a7
amSahar <53104527+amSahar@users.noreply.github.com>
2019-09-09 02:25:58 +0300

    Add files via upload

---
+++ lesson 14.py
@@ -0,0 +1,24 @@
+'''
+employee1=['Sahar ','Engineer ','43*****','0555555555']
+employee2=['Reem ','Accountant','43*****','0555555555']
+
+print(employee[0:4])
+
+
+if "Engineer" in employee:
+    print ("yes, he/she works in IT department")
+
+
+print (employee[0] * 4)
+
+
+employee=employee1[1]+employee2[1]
+print (employee)
+'''
+
+Decimal_Numbers=['3.14' , '1.03']
+Integer_Numbers=['-100' , '34']
+
+merge=Decimal_Numbers+Integer_Numbers
+
+print (merge)
