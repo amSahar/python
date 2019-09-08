cbd7711c66696c7a4507d3cd0b273257d0b0ca7c
amSahar <53104527+amSahar@users.noreply.github.com>
2019-09-09 00:55:20 +0300

    Add files via upload

---
+++ Lesson 13.py
@@ -0,0 +1,15 @@
+Decimal_Number=[ 1.44 , 3.14 , 6.32 ]
+
+'''
+print(Decimal_Number)
+print(Decimal_Number[1])
+for n in Decimal_Number:
+    print(n)
+
+Decimal_Number[2]= 2.15
+print(Decimal_Number)
+'''
+
+del Decimal_Number[0]
+del Decimal_Number[0]
+print (Decimal_Number)
