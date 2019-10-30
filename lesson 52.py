247a9e064b7a119bfb1d778d6dabf1eceb8aeeca
amSahar <53104527+amSahar@users.noreply.github.com>
2019-10-30 04:53:49 +0300

    Add files via upload

---
+++ lesson 52.py
@@ -0,0 +1,7 @@
+import datetime
+print(datetime.datetime.now())
+print(datetime.datetime.now().year)
+print(datetime.datetime.now().strftime('%A'))
+print(datetime.datetime(2030,1,1,1,1,1))
+print(datetime.datetime(2030,4,1,1,1,1).strftime('%W'))
+
