02c3f142be2a643b15528006f25976b99d3d8dcd
amSahar <53104527+amSahar@users.noreply.github.com>
2019-11-07 03:57:56 +0300

    Add files via upload

---
+++ lesson 67 68.py
@@ -0,0 +1,11 @@
+
+F_letter=input('Enter your first name letter: ')
+L_letter=input('Enter your last name letter: ')
+print('Your first letter is ',F_letter,'and last letter is ', L_letter)
+
+
+name='Ahmad Ali'
+balance='53.44'
+txt='Dear {1} , Your current balance is {0} $'
+print(txt.format(balance,name))
+
