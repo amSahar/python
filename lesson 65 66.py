0c40e34704c855c92457e678bb7ef7adc4aa1b85
amSahar <53104527+amSahar@users.noreply.github.com>
2019-11-07 03:57:33 +0300

    Add files via upload

---
+++ lesson 65 66.py
@@ -0,0 +1,6 @@
+name=input("Enter your name: ")
+year=int(input("Enter your birthday's year: "))
+cal=2019-year
+txt="Hello {0}, Your birthday's year is {1} so your age is: {2} years."
+print(txt.format(name,year,cal))
+
