1e095b028a84f7162f8536f9e601482a1d794455
amSahar <53104527+amSahar@users.noreply.github.com>
2019-11-07 03:57:05 +0300

    Add files via upload

---
+++ lesson 64.py
@@ -0,0 +1,33 @@
+try:
+    print(x)
+except:
+    print("An exception occurred")
+
+
+
+
+
+try:
+    print (x)
+except NameError:
+    print('Varibal x is not defined')
+except:
+    print('Something else went wrong')
+
+try:
+    print('Computer Engineering')
+except:
+    print('Something went wrong')
+else:
+    print('Nothing went wrong')
+
+
+
+
+
+try:
+    print(x)
+except:
+    print("Something went wrong")
+finally:
+    print("The 'try except' is finished")
